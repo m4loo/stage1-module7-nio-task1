4c1eb882f1643ccbbcd63bb45a5dfc4524cf2f97
m4loo <128167781+m4loo@users.noreply.github.com>
2023-08-05 21:51:27 +0600

    done

--- src/main/java/com/epam/mjc/nio/FileReader.java
+++ src/main/java/com/epam/mjc/nio/FileReader.java
@@ -1,11 +1,48 @@
 package com.epam.mjc.nio;
 
-import java.io.File;
-
+import java.io.*;
+import java.nio.file.*;
 
 public class FileReader {
 
     public Profile getDataFromFile(File file) {
-        return new Profile();
+        String name = null;
+        int age = 0;
+        String email = null;
+        long phone = 0;
+
+        Path path = Paths.get(file.getPath());
+
+        try (BufferedReader bufferedReader = Files.newBufferedReader(path)) {
+            String line;
+            while ((line = bufferedReader.readLine()) != null) {
+                String[] parts = line.split(": ");
+                String key = parts[0];
+                String value = parts[1];
+                if (parts.length == 2) {
+                    switch (key) {
+                        case "Name":
+                            name = value;
+                            break;
+                        case "Age":
+                            age = Integer.parseInt(value);
+                            break;
+                        case "Email":
+                            email = value;
+                            break;
+                        case "Phone":
+                            phone = Long.parseLong(value);
+                            break;
+                        default:
+                            break;
+                    }
+                }
+            }
+        } catch (IOException e){
+            e.printStackTrace();
+            return null;
+        }
+
+        return new Profile(name, age, email, phone);
     }
 }
